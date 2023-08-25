cb9f0b07d0a344c14f670df654b47d965bedeb31
Clamwash <clam.wash@gmail.com>
2023-08-25 12:57:55 +0200

    add updateConjuredItem method

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -20,7 +20,11 @@ public class GildedRose {
         case "Backstage passes to a TAFKAL80ETC concert":
             updateBackstagePasses(item)
         default:
-            updateRegularItem(item)
+            if item.name.contains("Conjured") {
+                updateConjuredItem(item)
+            } else {
+                updateRegularItem(item)
+            }
         }
     }
 
@@ -64,5 +68,15 @@ public class GildedRose {
         if item.sellIn < 0 && item.quality > 0 {
             item.quality -= 1
         }
+    }
+    
+    private func updateConjuredItem(_ item: Item) {
+        let degradeAmount = 2
+        if item.quality > degradeAmount {
+            item.quality -= degradeAmount
+        } else {
+            item.quality = 0
+        }
+        item.sellIn -= 1
     }
 }
